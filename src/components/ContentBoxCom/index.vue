62465b9f7ae7d74422144f405c6cbea09d3666fb
ZhBano <mozbano@163.com>
2021-03-25 10:09:33 +0800

    添加ContentBoxCom组件

---
+++ src/components/ContentBoxCom/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <view class="ContentBoxCom">
+    <template v-if="$slots.default">
+      <slot></slot>
+    </template>
+    <template v-else>
+      <slot name="noData">
+        <view class="nodata">暂无数据</view>
+      </slot>
+    </template>
+  </view>
+</template>
+<script>
+export default {
+  props: {},
+  created() {
+    console.log(this, "this");
+  },
+};
+</script>
+<style lang="scss" scoped>
+.ContentBoxCom {
+  height: 100%;
+}
+</style>

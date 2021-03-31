ce59fab7d90ec217a5294f836119818dd8c943d2
ZhBano <mozbano@163.com>
2021-03-31 16:12:13 +0800

    Revert "部分promise替换callback"
    This reverts commit 3a90ec0a977144b0f9b8850e73529d093bb2f68a.

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,4 +1,4 @@
-import WXDecode from "@/utils/WXBizDataCrypt";
+import wxDecode from "@/utils/WXBizDataCrypt";
 import * as utils from '@/utils'
 
 import {
@@ -79,28 +79,28 @@ export default {
 		},
 
 		//提示框
-		message(title, duration = 1500) {
+		message(title,duration=1500) {
 			uni.showToast({
 				title,
 				icon: 'none',
 				duration
 			})
 		},
-
+		
 		//模态框
-		messageBox(object = { title: '', content: '', showCancel: true }) {
+		messageBox(object={title:'',content:'',showCancel:true}) {
 			return new Promise((resolve, reject) => {
 				uni.showModal({
-					title: object.title,
-					content: object.content,
-					showCancel: object.showCancel,
+					title:object.title,
+					content:object.content,
+					showCancel:object.showCancel,
 					success: success => {
-						if (success.confirm) {
+						if(success.confirm){
 							// 确定
 							resolve()
 						}
-
-						if (success.cancel) {
+						
+						if(success.cancel){
 							// 取消
 							reject()
 						}
@@ -110,13 +110,13 @@ export default {
 					}
 				})
 			})
-
+		
 		},
-
-
-
+		
+		
+		
 		// 腾讯地图-地址解析
-		geocoder(key, address) {
+		geocoder(key,address){
 			return new Promise((resolve, reject) => {
 				new utils.MapWx(key).geocoder(address).then(res => {
 					resolve(res)
@@ -124,19 +124,19 @@ export default {
 					reject(err)
 				})
 			})
-		},
-
-		// 腾讯地图-逆地址解析
-		reverseGeocoder(key, location) {
-			return new Promise((resolve, reject) => {
-				new utils.MapWx(key).reverseGeocoder(location).then(res => {
-					resolve(res)
-				}).catch(err => {
-					reject(err)
+			},
+			
+			// 腾讯地图-逆地址解析
+			reverseGeocoder(key, location) {
+				return new Promise((resolve, reject) => {
+					new utils.MapWx(key).reverseGeocoder(location).then(res => {
+						resolve(res)
+					}).catch(err => {
+						reject(err)
+					})
 				})
-			})
-		},
-
+			},
+		
 		//打电话
 		callPhone(phoneNumber) {
 			uni.makePhoneCall({
@@ -155,19 +155,20 @@ export default {
 		},
 
 		// 打开地图-用于搜索地址，选择位置
-		searchLocation(longitude, latitude, callback) {
-			uni.chooseLocation({
-				longitude: Number(longitude), // 经度
-				latitude: Number(latitude), // 纬度
-				success: res => {
-					if (typeof callback === 'function') {
-						callback(res)
+		searchLocation(longitude, latitude) {
+			return new Promise((resolve, reject) => {
+
+				uni.chooseLocation({
+					longitude, // 经度
+					latitude, // 纬度
+					success: res => {
+						resolve(res)
+					},
+					fail:err=>{
+						reject(err)
 					}
-
-				},
-
+				})
 			})
-
 		},
 
 
@@ -220,7 +221,7 @@ export default {
 			})
 		},
 
-
+		
 
 		// 图片处理-预览图片
 		previewImage(urls = [], current = 0) {
@@ -236,69 +237,67 @@ export default {
 		},
 
 		// 图片处理-获取图片信息
-		getImageInfo(src = '', callback) {
-			uni.getImageInfo({
-				src: src,
-				success: (image) => {
-					if (typeof callback === 'function') {
-						callback(image)
-					}
-
-				},
-				fail(err) {
-					console.log('getImageInfo出错', src, err)
-				},
+		getImageInfo(src = '') {
+			return new Promise((resolve, reject) => {
+				uni.getImageInfo({
+					src: src,
+					success: (image) => {
+						resolve(image)
+					},
+					fail(err) {
+						console.log('getImageInfo出错', src, err)
+					},
+				})
+			}).catch(e => {
+				reject(e)
 			})
-
 
 		},
 
 
 
 		// 微信登录
-		wxLogin(callback) {
-			uni.login({
-				provider: 'weixin',
-				success: data => {
-					if (typeof callback === 'function') {
-						callback(data.code)
+		wxLogin() {
+			return new Promise((resolve, reject) => {
+				uni.login({
+					provider: 'weixin',
+					success: data => {
+						resolve(data.code)
 					}
+				});
+			})
 
-				}
-			});
 		},
 
 		// 获取位置
-		getLocation(callback) {
-			uni.authorize({
-				scope: 'scope.userLocation',
-				success: _ => {
-					uni.getLocation({
-						type: 'gcj02',
-						success: res => {
-							if (typeof callback === 'function') {
-								callback(res)
+		getLocation() {
+			return new Promise((resolve, reject) => {
+				uni.authorize({
+					scope: 'scope.userLocation',
+					success: res => {
+						uni.getLocation({
+							type: 'gcj02',
+							success: res => {
+								resolve(res)
 							}
+						});
+					},
 
-						}
-					});
-				},
+					fail: err => {
+						// this.showModal({
+						// 	title: '提示',
+						// 	content: '建议获取定位授权'
+						// }).then(res => {
+						// 	uni.openSetting({
+						// 		success(res) {
 
-				fail: err => {
-					// this.showModal({
-					// 	title: '提示',
-					// 	content: '建议获取定位授权'
-					// }).then(res => {
-					// 	uni.openSetting({
-					// 		success(res) {
-
-					// 		}
-					// 	})
-					// })
-					this.message('请点击右上角...进行授权')
-				}
+						// 		}
+						// 	})
+						// })
+						this.message('请点击右上角...进行授权')
+					}
+				})
 			})
-
 
 		},
 
@@ -315,7 +314,7 @@ export default {
 				return false
 			}
 		},
-
+		
 		/**
 		 * 表单验证是否为空，只支持字符串
 		 * @param {Object} fields 需要验证的对象
@@ -333,22 +332,22 @@ export default {
 					return;
 				}
 			}
-
+		
 			if (isTrue) {
 				callback();
 			}
 		},
-
+		
 
 		//时间戳转日期格式
-		parseTime(date = new Date(), format = '{y}-{m}-{d}') {
+		parseTime(date=new Date(), format = '{y}-{m}-{d}') {
 			return utils.parseTime(date, format)
 		},
 
 
-		// 解密
+		//步数解密
 		wxDecode(data) {
-			let pc = new WXDecode(data.appId, data.sessionKey)
+			let pc = new wxDecode(data.appId, data.sessionKey)
 			let val = pc.decryptData(data.encryptedData, data.iv)
 			return val
 		}

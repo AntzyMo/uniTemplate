88ece76de10df993c777a0b66f6d5e192a7b4f02
ZhBano <mozbano@163.com>
2021-06-10 18:30:10 +0800

    修改内容组件

--- src/components/ContentBoxCom/index.vue
+++ src/components/ContentBoxCom/index.vue
@@ -1,6 +1,6 @@
 <template>
   <view class="ContentBoxCom">
-    <template v-if="$slots.default">
+    <template v-if="data">
       <slot></slot>
     </template>
     <template v-else>
@@ -12,10 +12,13 @@
 </template>
 <script>
 export default {
-  props: {},
-  created() {
-    console.log(this, "this");
+  props: {
+     data:{
+      type:[Boolean,Number,String],
+      default:false
+    }
   },
+ 
 };
 </script>
 <style lang="scss" scoped>
